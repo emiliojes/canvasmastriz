48e70d60f7c5943f8c40fbb1512a50aa7d14c8bc
Emilio Nuñez <emiliojes@gmail.com>
2017-10-09 21:18:18 -0500

    Añadiendo matriz canvas en la parte superior derecha

--- dibujo.js
+++ dibujo.js
@@ -2,20 +2,26 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var lineas = 30;
 //var l = 0;
-var yi, xf;
+var xi, yi, xf, yf;
 var colorcito = "#FAA";
 
 //while(l < lineas)//mientras algo sea verdad va ocurrir algo
 for(l = 0; l < lineas; l++)
 {
+	xi = 10 * l;
+	yf = 10 * (l + 1);
 	yi = 10 * l;
 	xf = 10 * (l + 1);
 	dibujarLinea(colorcito, 0, yi, xf, 300);
-	console.log("linea " + l); 
+	dibujarLinea(colorcito, xi, 0, 300, yf);
+	console.log("linea " + xi); 
 //	l++;
 }
 dibujarLinea(colorcito, 1, 1, 1, 300);
 dibujarLinea(colorcito, 1, 299, 299, 299);
+//dibujarLinea(colorcito, 0, 0, 300, 10);
+//dibujarLinea(colorcito, 10, 0, 300, 20);
+//dibujarLinea(colorcito, 20, 0, 300, 30);
 
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
